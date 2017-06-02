536d71ff4d767c22bac9ba416eeff564fd7b9985
draven05 <draven05@naver.com>
2017-06-02 23:05:28 +0900

    add metho print

--- Myproject02/src/co/kr/draven05/common/CommonTest.java
+++ Myproject02/src/co/kr/draven05/common/CommonTest.java
@@ -3,6 +3,11 @@ package co.kr.draven05.common;
 public class CommonTest {
 
 	public static void main(String[] args) throws Exception {
+		CommonTest.print();
+	}
+
+	// sub1
+	public static void print() {
 		System.out.println("Hello~~~~");
 	}
 }
