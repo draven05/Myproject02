2c3eda840885fd9ef4f1de29cfed6539730e0e77
draven05 <draven05@naver.com>
2017-06-02 23:12:01 +0900

    text로 변경

--- Myproject02/src/co/kr/draven05/common/CommonTest.java
+++ Myproject02/src/co/kr/draven05/common/CommonTest.java
@@ -2,12 +2,14 @@ package co.kr.draven05.common;
 
 public class CommonTest {
 
+	public static String text = "Hello~~~~~~";
+	
 	public static void main(String[] args) throws Exception {
 		CommonTest.print();
 	}
 
 	// sub1
 	public static void print() {
-		System.out.println("Hello~~~~");
+		System.out.println(CommonTest.text);
 	}
 }
